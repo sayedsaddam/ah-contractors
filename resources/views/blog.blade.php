7d7619bf2dff9d9e65724eff3886f8011026161a
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2021-08-20 18:01:56 +0500

    Created new file inside resources views's folder for blog.

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,121 @@
+@include('layouts/head')
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <div class="overlay"></div>
+        <div class="container">
+            <div class="row no-gutters slider-text align-items-end justify-content-start">
+                <div class="col-md-9 ftco-animate pb-5">
+                <p class="breadcrumbs"><span class="mr-2"><a href="/">Home <i class="fa fa-chevron-right"></i></a></span> <span>Our Blog <i class="fa fa-chevron-right"></i></span></p>
+                <h1 class="mb-3 bread">Our Blog</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="ftco-section bg-light">
+    <div class="container">
+        <div class="row d-flex">
+            <div class="col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');"></a>
+                    <div class="text d-block">
+                        <div class="meta">
+                            <p>
+                                <a href="#"><span class="fa fa-calendar mr-2"></span>Sept. 06, 2020</a>
+                                <a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+                                <a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+                            </p>
+                        </div>
+                        <h3 class="heading"><a href="#">Best for any industrial &amp; business solution</a></h3>
+                        <p><a href="blog.html" class="btn btn-secondary py-2 px-3">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_2.jpg');"></a>
+                    <div class="text d-block">
+                        <div class="meta">
+                            <p>
+                                <a href="#"><span class="fa fa-calendar mr-2"></span>Sept. 06, 2020</a>
+                                <a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+                                <a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+                            </p>
+                        </div>
+                        <h3 class="heading"><a href="#">Best for any industrial &amp; business solution</a></h3>
+                        <p><a href="blog.html" class="btn btn-secondary py-2 px-3">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');"></a>
+                    <div class="text d-block">
+                        <div class="meta">
+                            <p>
+                                <a href="#"><span class="fa fa-calendar mr-2"></span>Sept. 06, 2020</a>
+                                <a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+                                <a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+                            </p>
+                        </div>
+                        <h3 class="heading"><a href="#">Best for any industrial &amp; business solution</a></h3>
+                        <p><a href="blog.html" class="btn btn-secondary py-2 px-3">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_4.jpg');"></a>
+                    <div class="text d-block">
+                        <div class="meta">
+                            <p>
+                                <a href="#"><span class="fa fa-calendar mr-2"></span>Sept. 06, 2020</a>
+                                <a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+                                <a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+                            </p>
+                        </div>
+                        <h3 class="heading"><a href="#">Best for any industrial &amp; business solution</a></h3>
+                        <p><a href="blog.html" class="btn btn-secondary py-2 px-3">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_5.jpg');"></a>
+                    <div class="text d-block">
+                        <div class="meta">
+                            <p>
+                                <a href="#"><span class="fa fa-calendar mr-2"></span>Sept. 06, 2020</a>
+                                <a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+                                <a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+                            </p>
+                        </div>
+                        <h3 class="heading"><a href="#">Best for any industrial &amp; business solution</a></h3>
+                        <p><a href="blog.html" class="btn btn-secondary py-2 px-3">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 ftco-animate">
+                <div class="blog-entry">
+                    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_6.jpg');"></a>
+                    <div class="text d-block">
+                        <div class="meta">
+                            <p>
+                                <a href="#"><span class="fa fa-calendar mr-2"></span>Sept. 06, 2020</a>
+                                <a href="#"><span class="fa fa-user mr-2"></span>Admin</a>
+                                <a href="#" class="meta-chat"><span class="fa fa-comment mr-2"></span> 3</a>
+                            </p>
+                        </div>
+                        <h3 class="heading"><a href="#">Best for any industrial &amp; business solution</a></h3>
+                        <p><a href="blog.html" class="btn btn-secondary py-2 px-3">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@include('layouts/foot')
